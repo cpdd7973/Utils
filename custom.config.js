170845a98c13b7f0c1e4aab1da27d103f6e21c78
cpdd7973 <chanderp756@gmail.com>
2024-08-17 01:28:52 +0530

    update

--- custom.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  paths: {
-    '/index.html': '/', // Change this if your index file has a different name
-  },
-  publicPath: '/', // Replace with your GitHub Pages deployment path
-};
